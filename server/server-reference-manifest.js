2a773242d68267a14e5726e075a7f20e58209d79
liuwenjun05 <liuwenjun05@meituan.com>
2023-12-25 20:19:22 +0800

    Updates

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"H1cta+yh5iXSv0QADy9wPxzUtmSMm8nlsAnKhtobDT4=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IGb5uVqv7QJNnU7Z/+dhtxE53NrHqNINAvpeZKAlH/w=\"}"
